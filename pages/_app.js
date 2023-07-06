6a2b7560f4286e2c65784e9ef32d0b1ca581bb63
Frank Schaeske <frank.schaeske@web.de>
2023-07-06 13:02:03 +0200

    work on user story 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+import { useState } from "react";
+import useSWR from "swr";
 
 export default function App({ Component, pageProps }) {
+  async function fetcher() {
+    const response = await fetch("https://example-apis.vercel.app/api/art");
+    const parsedJson = await response.json();
+    return parsedJson;
+  }
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+  console.log("Data", data);
+
+  const [globalData, setGlobalData] = useState(data);
+
+  if (error !== undefined) {
+    return <h2>{error.message}</h2>;
+  }
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <>
+    <SWRConfig value={{ fetcher, refreshInterval: 1000 }}>
       <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+      <Component {...pageProps} globalData={globalData} />
+    </SWRConfig>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,11 @@ import ArtPieces from "@/components/ArtPieces";
 import ArtPiecePreview from "@/components/ArtPiecePreview";
 import Link from "next/link";
 
-export default function HomePage() {
-  async function fetcher() {
-    const response = await fetch("https://example-apis.vercel.app/api/art");
-    const parsedJson = await response.json();
-    console.log(parsedJson);
-    return parsedJson;
-  }
-  const { data, error, isLoading } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
-  );
-  console.log(data);
+export default function HomePage({ globalData }) {
+  const pieces = globalData;
+  console.log("globalData", globalData);
 
-  if (error !== undefined) {
-    return <h2>{error.message}</h2>;
-  }
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  }
-  const pieces = data;
-  const name = data.map((artPiece) => {
+  /*const name = data.map((artPiece) => {
     return artPiece.name;
   });
   const title = data.map((artPiece) => {
@@ -32,7 +16,8 @@ export default function HomePage() {
   });
   const image = data.map((artPiece) => {
     return artPiece;
-  });
+  });*/
+
   return (
     <>
       {" "}
