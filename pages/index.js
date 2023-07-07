1da24e84b45ec40974393d2ce1f926c126e0c96c
Jonas Hinzmann <jonas.hinzmann@htp-tel.de>
2023-07-07 09:10:19 +0200

    fix bugs

--- pages/index.js
+++ pages/index.js
@@ -5,41 +5,12 @@ import ArtPiecePreview from "@/components/ArtPiecePreview";
 import Link from "next/link";
 import Spotlight from "@/components/Spotlight";
 
-
-export default function SpotlightPage() {
-
-export default function HomePage() {
-  async function fetcher() {
-    const response = await fetch("https://example-apis.vercel.app/api/art");
-    const parsedJson = await response.json();
-    console.log(parsedJson);
-    return parsedJson;
-  }
-  const { data, error, isLoading } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
-  );
-  console.log(data);
-
+export default function SpotlightPage({ pieces }) {
   function getRandomPiece() {
-    const length = data.length;
+    const length = pieces.length;
     const randomIndex = Math.floor(Math.random() * length);
-    return data[randomIndex];
+    return pieces[randomIndex];
   }
-
-  if (error !== undefined) {
-    return <h2>{error.message}</h2>;
-  }
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  }
-  const pieces = data;
-  const name = data.map((artPiece) => {
-    return artPiece.name;
-  });
-  const title = data.map((artPiece) => {
-    return artPiece.title;
-  });
 
   const randomPiece = getRandomPiece();
 
@@ -47,9 +18,6 @@ export default function HomePage() {
     <>
       <h1>Art Gallery</h1>
 
-      {/*<Spotlight image={placeholder} artist={placeholder} />*/}
-
-      <ArtPieces pieces={pieces} />
       <Spotlight
         image={randomPiece.imageSource}
         artist={randomPiece.artist}
